ca49e924c1706390d1543be7ced72fe1cf78881d
Dean Irwin <Deani1developer@outlook.com>
2020-04-01 15:50:26 -0400

    Project Init

---
+++ src/main/java/co/deanirwin/sfgspringpetclinic/SfgSpringPetClinicApplication.java
@@ -0,0 +1,13 @@
+package co.deanirwin.sfgspringpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgSpringPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SfgSpringPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/deanirwin/sfgspringpetclinic/SfgSpringPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package co.deanirwin.sfgspringpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgSpringPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
