94af2022362b8be203141b94292dc4dd00fd8cb1
Dean Irwin <Deani1developer@outlook.com>
2020-04-03 16:03:42 -0400

    Added a comment for myself regarding JPA spec and best practice.

--- pet-clinic-data/src/main/java/co/deanirwin/sfgspringpetclinic/models/BaseEntity.java
+++ pet-clinic-data/src/main/java/co/deanirwin/sfgspringpetclinic/models/BaseEntity.java
@@ -5,8 +5,9 @@ import lombok.Setter;
 
 import java.io.Serializable;
 
+//It is best practice to keep ID generation in a common base class in case ID generation
+//strategy ever changes. It is preferred by JPA that detached objects implement serializable.
+
 public class BaseEntity implements Serializable {
     @Getter @Setter private Long id;
-
-
 }
