106b93a690df4fa1723a36f8c4d63934c070a28a
Dean Irwin <Deani1developer@outlook.com>
2020-04-04 09:26:35 -0400

    Added index page and controllers for vets and pet owners. Closes #7. Closes #13.

--- pet-clinic-web/src/main/java/co/deanirwin/sfgspringpetclinic/controllers/PetOwnerController.java
+++ pet-clinic-web/src/main/java/co/deanirwin/sfgspringpetclinic/controllers/PetOwnerController.java
@@ -1,8 +1,13 @@
 package co.deanirwin.sfgspringpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PetOwnerController {
 
+    @RequestMapping("/pet-owners")
+    public String index() {
+        return "pet_owners/index";
+    }
 }
--- pet-clinic-web/src/main/java/co/deanirwin/sfgspringpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/co/deanirwin/sfgspringpetclinic/controllers/VetController.java
@@ -1,8 +1,13 @@
 package co.deanirwin.sfgspringpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    @RequestMapping("/vets")
+    public String index() {
+        return "vets/index";
+    }
 }
