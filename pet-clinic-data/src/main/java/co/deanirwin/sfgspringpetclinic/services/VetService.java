27638e44abf630364bc3e5ceafa9ab40c5acadf3
Dean Irwin <Deani1developer@outlook.com>
2020-04-04 22:20:31 -0400

    Created data based index page for vets. Closes #15

--- pet-clinic-data/src/main/java/co/deanirwin/sfgspringpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/co/deanirwin/sfgspringpetclinic/services/VetService.java
@@ -1,7 +1,9 @@
 package co.deanirwin.sfgspringpetclinic.services;
 
 import co.deanirwin.sfgspringpetclinic.models.Vet;
+import org.springframework.stereotype.Service;
 
+@Service
 public interface VetService extends CrudService<Vet, Long> {
 
     Vet findByLastName(String lastName);
--- pet-clinic-web/src/main/java/co/deanirwin/sfgspringpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/co/deanirwin/sfgspringpetclinic/controllers/VetController.java
@@ -1,13 +1,23 @@
 package co.deanirwin.sfgspringpetclinic.controllers;
 
+import co.deanirwin.sfgspringpetclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    //Autowired
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping("/vets")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vets/index";
     }
 }
