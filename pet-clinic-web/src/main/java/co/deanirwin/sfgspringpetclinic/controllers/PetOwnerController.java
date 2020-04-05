9bba76163dbd70ba55269719627cd0befea23fb1
Dean Irwin <Deani1developer@outlook.com>
2020-04-04 22:02:21 -0400

    Created data based index page for pet owners. Closes #16

--- pet-clinic-web/src/main/java/co/deanirwin/sfgspringpetclinic/controllers/PetOwnerController.java
+++ pet-clinic-web/src/main/java/co/deanirwin/sfgspringpetclinic/controllers/PetOwnerController.java
@@ -1,13 +1,27 @@
 package co.deanirwin.sfgspringpetclinic.controllers;
 
+import co.deanirwin.sfgspringpetclinic.services.PetOwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class PetOwnerController {
 
+    private final PetOwnerService petOwnerService;
+
+    //Autowired
+    public PetOwnerController(PetOwnerService petOwnerService) {
+        this.petOwnerService = petOwnerService;
+    }
+
     @RequestMapping("/pet-owners")
-    public String index() {
+    public String index(Model model) {
+
+        model.addAttribute("petOwners", petOwnerService.findAll());
+
         return "pet_owners/index";
     }
+
+
 }
